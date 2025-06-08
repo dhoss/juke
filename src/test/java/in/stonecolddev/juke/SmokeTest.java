5e3642ea894971bb8834b7aeb7ded0a2db3676d0
Devin Austin <devin.austin@gmail.com>
2025-06-08 14:19:58 -0600

    * Renamed JukeApplicationTests to SmokeTest
      * Added nullness check test for HomeController instance

--- src/test/java/in/stonecolddev/juke/SmokeTest.java
+++ src/test/java/in/stonecolddev/juke/SmokeTest.java
@@ -1,51 +1,54 @@
 package in.stonecolddev.juke;
 
+import in.stonecolddev.juke.ui.HomeController;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 import static in.stonecolddev.juke.util.DbMigrations.initDb;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ActiveProfiles("it-test")
 @SpringBootTest
-class JukeApplicationTests {
+public class SmokeTest {
 
-	static String username = "juke";
-	static String password = "juke";
-	static String databaseName = "juke";
+	private static String username = "juke";
+	private static String password = "juke";
+	private static String databaseName = "juke";
+
+	@Autowired
+	private HomeController homeController;
 
 	@Container
-	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+	private static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
 			.withUsername(username)
 			.withPassword(password)
 			.withDatabaseName(databaseName);
 
 
 	@BeforeAll
-	static void beforeAll() {
+	public static void beforeAll() {
 		postgres.start();
 		System.setProperty("spring.datasource.hikari.jdbc-url", postgres.getJdbcUrl());
 		System.setProperty("spring.datasource.hikari.username", postgres.getUsername());
 		System.setProperty("spring.datasource.hikari.password", postgres.getPassword());
 
-
 		initDb(postgres.getJdbcUrl(), username, password);
 	}
 
 	@AfterAll
-	static void afterAll() {
+	public static void afterAll() {
 		postgres.stop();
 	}
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(homeController).isNotNull();
 	}
 
 }

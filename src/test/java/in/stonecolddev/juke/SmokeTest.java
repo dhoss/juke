1a1331d212511aa0f09b22bc023093549f0b4a42
Devin Austin <devin.austin@gmail.com>
2025-08-17 13:32:12 -0600

    Fixed integration tests, hopefully

--- src/test/java/in/stonecolddev/juke/SmokeTest.java
+++ src/test/java/in/stonecolddev/juke/SmokeTest.java
@@ -5,18 +5,22 @@ import in.stonecolddev.juke.ui.HomeController;
 import in.stonecolddev.juke.ui.page.PageController;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static in.stonecolddev.juke.util.Fixtures.Database.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@Testcontainers
 @ActiveProfiles("it-test")
 @SpringBootTest
+@Tag("it-test")
 public class SmokeTest {
 
 
